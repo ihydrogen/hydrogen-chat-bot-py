f00dc359230816a9336b5cdb641270f82de70570
ihydrogen <taleb4808@gmail.com>
2017-02-18 23:56:29 +0300

    Update load.py

--- console/modules/load.py
+++ console/modules/load.py
@@ -8,7 +8,7 @@ def main(c):
     module_name = module_name.strip()
 
     if not module_name:
-        raise Exception("%s 'name of modules' or '-a' for all" % CMAND)
+        raise Exception("%s 'name of module' or '-a' for all" % CMAND)
 
     m = mpm_manager.ModuleManager()
     if module_name == "-a":
